291abde9628082c2ad049278772d67f5349a0f6e
DavuKX <duvandavid2001@gmail.com>
2022-07-17 13:56:28 -0500

    add game function and show score

--- static/js/main.js
+++ static/js/main.js
@@ -11,22 +11,39 @@ function getComputerChoice(CHOICES) {
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = CHOICES[playerSelection];
+    // playerSelection = CHOICES[playerSelection];
     if ((playerSelection - computerSelection) === 0) {
-        return "Is a tie!"
+        return 0; // 0 for tie
     } else if ((playerSelection - computerSelection) === 1) {
-        return `You lose! ${getKeyFromValue(computerSelection)} beats ${getKeyFromValue(playerSelection)}`;
+        return 1; // 1 for lose
     } else if ((playerSelection - computerSelection) === -1) {
-        return `You win! ${getKeyFromValue(playerSelection)} beats ${getKeyFromValue(computerSelection)}`;
+        return 2; // 2 for win
     } else if ((playerSelection - computerSelection) === -2) {
-        return `You lose! ${getKeyFromValue(computerSelection)} beats ${getKeyFromValue(playerSelection)}`;
+        return 1;
     } else {
-        return `You win! ${getKeyFromValue(playerSelection)} beats ${getKeyFromValue(computerSelection)}`;
+        return 2;
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection))
+function game() {
+    let playerSelection;
+    let gamesWon = 0;
+    for (let i = 0; i < 5; i++) {
+        computerSelection = getComputerChoice()
+        playerSelection = prompt("Type in your choice: ").toLowerCase()
+        while (!(playerSelection in CHOICES)) {
+            playerSelection = prompt("Type a correct choice (paper, rock, scissors): ").toLowerCase()
+        }
+        if (playRound(CHOICES[playerSelection], computerSelection) === 0) {
+            console.log("It's a tie!")
+        } else if (playRound(CHOICES[playerSelection], computerSelection) === 1) {
+            console.log(`You lose! ${getKeyFromValue(computerSelection)} beats ${playerSelection}`)
+        } else if (playRound(CHOICES[playerSelection], computerSelection) === 2) {
+            gamesWon += 1;
+            console.log(`You win! ${playerSelection} beats ${getKeyFromValue(computerSelection)}`)
+        }
+    }
+    console.log(`You won ${gamesWon} out of 5 games.`)
+}
 
-    
+game()
